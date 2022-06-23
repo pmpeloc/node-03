637ef0ee65adfcfa388dc72708ab9107b78365b5
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-06-23 20:40:59 -0300

    feat: Refresh token route added

--- auth/auth.routes.config.ts
+++ auth/auth.routes.config.ts
@@ -1,9 +1,11 @@
+import express from 'express';
+import { body } from 'express-validator';
+
 import { CommonRoutesConfig } from '../common/common.routes.config';
 import authController from './controllers/auth.controller';
 import authMiddleware from './middleware/auth.middleware';
-import express from 'express';
 import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
-import { body } from 'express-validator';
+import jwtMiddleware from './middleware/jwt.middleware';
 
 export class AuthRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
@@ -18,6 +20,14 @@ export class AuthRoutes extends CommonRoutesConfig {
       authMiddleware.verifyUserPassword,
       authController.createJWT,
     ]);
+
+    this.app.post(`/auth/refresh-token`, [
+      jwtMiddleware.validJWTNeeded,
+      jwtMiddleware.verifyRefreshBodyField,
+      jwtMiddleware.validRefreshNeeded,
+      authController.createJWT,
+    ]);
+
     return this.app;
   }
 }
