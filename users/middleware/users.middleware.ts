7b6086dd2a3b0104cc58d543602a587c30caf864
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-06-23 21:06:22 -0300

    feat: Permissions added

--- users/middleware/users.middleware.ts
+++ users/middleware/users.middleware.ts
@@ -36,8 +36,7 @@ class UsersMiddleware {
     res: express.Response,
     next: express.NextFunction
   ) {
-    const user = await userService.getUserByEmail(req.body.email);
-    if (user && user._id === req.params.userId) {
+    if (res.locals.user._id === req.params.userId) {
       next();
     } else {
       res.status(400).send({ error: `Invalid email` });
@@ -66,6 +65,7 @@ class UsersMiddleware {
   ) {
     const user = await userService.readById(req.params.userId);
     if (user) {
+      res.locals.user = user;
       next();
     } else {
       res.status(404).send({
@@ -81,6 +81,23 @@ class UsersMiddleware {
   ) {
     req.body.id = req.params.userId;
     next();
+  }
+
+  async userCantChangePermission(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (
+      'permissionFlags' in req.body &&
+      req.body.permissionFlags !== res.locals.user.permissionFlags
+    ) {
+      res.status(400).send({
+        errors: ['User cannot change permission flags'],
+      });
+    } else {
+      next();
+    }
   }
 }
 
