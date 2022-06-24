1f9c4fdbb115276c9dd3090f11d1cbc6f639dc43
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-06-23 21:31:14 -0300

    feat: Proyecto finalizado

--- users/dao/users.dao.ts
+++ users/dao/users.dao.ts
@@ -5,6 +5,7 @@ import mongooseService from '../../common/services/mongoose.service';
 import { CreateUserDto } from '../dto/create.user.dto';
 import { PatchUserDto } from '../dto/patch.user.dto';
 import { PutUserDto } from '../dto/put.user.dto';
+import { PermissionFlag } from '../../common/middleware/common.permissionflag.enum';
 
 const log: debug.IDebugger = debug('app:in-memory-dao');
 
@@ -36,7 +37,7 @@ class UsersDao {
     const user = new this.User({
       _id: userId,
       ...userFields,
-      permissionFlags: 1,
+      permissionFlags: PermissionFlag.FREE_PERMISSION,
     });
     await user.save();
     return userId;
